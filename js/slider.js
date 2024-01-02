6a993250793e3741aec00eb072716b7ca130e029
Kirill <kirillsabaev93@gmail.com>
2024-01-03 01:10:06 +0400

    Готов блок для записи на урок

---
+++ js/slider.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const wrapTeachers = document.querySelector('.wrap__teachers');
+    const items = document.querySelectorAll('.item__teachers');
+    let currentIndex = 0;
+
+    function showItem(index) {
+        items.forEach((item, i) => {
+            const translateValue = (i - index) * (item.offsetWidth + 40) + 'px'; // Учитываем gap
+            item.style.transform = `translateX(${translateValue})`;
+        });
+    }
+
+    function move(direction) {
+        if (direction === 'left') {
+            currentIndex = (currentIndex - 1 + items.length) % items.length;
+        } else {
+            currentIndex = (currentIndex + 1) % items.length;
+        }
+        showItem(currentIndex);
+    }
+
+    document.querySelector('.right').addEventListener('click', function () {
+        move('right');
+    });
+
+    document.querySelector('.left').addEventListener('click', function () {
+        move('left');
+    });
+});
